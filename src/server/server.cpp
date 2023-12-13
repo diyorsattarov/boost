50844dbf94313ef213a81d621175f89a70247170
diyooor <diyorsattarov7@gmail.com>
2023-12-13 13:04:19 -0600

    Added: - spdlog to server loggign

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -2,15 +2,21 @@
 
 Server::Server(boost::asio::io_service& io_service, short port)
     : acceptor_(io_service, tcp::endpoint(tcp::v4(), port)),
-        socket_(io_service) {
+      socket_(io_service) {
+    Utilities::console_logger()->info("Server: Starting on port {}", port);
     doAccept();
 }
 
+
 void Server::doAccept() {
+    Utilities::console_logger()->info("Server: Waiting for new connection");
     acceptor_.async_accept(socket_,
         [this](boost::system::error_code ec) {
             if (!ec) {
+                Utilities::console_logger()->info("Server: New connection accepted");
                 std::make_shared<Session>(std::move(socket_))->start();
+            } else {
+                Utilities::console_logger()->error("Server Accept Error: {}", ec.message());
             }
 
             doAccept();
