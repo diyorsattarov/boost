edf21bb7dac593f38cc5f2f6fa1654821c52be01
diyooor <diyorsattarov7@gmail.com>
2023-12-13 13:03:55 -0600

    Added: - spdlog logging

--- src/session/session.cpp
+++ src/session/session.cpp
@@ -1,12 +1,16 @@
 #include <session/session.h>
 
-Session::Session(tcp::socket socket) : socket_(std::move(socket)) {}
+Session::Session(tcp::socket socket) : socket_(std::move(socket)) {
+    Utilities::console_logger()->info("Session: New session created");
+}
 
 void Session::start() {
+    Utilities::console_logger()->info("Session: Starting session");
     doRead();
 }
 
 void Session::doRead() {
+    Utilities::console_logger()->info("Session: Starting async_read_some");
     auto self(shared_from_this());
     socket_.async_read_some(boost::asio::buffer(data_, max_length),
         boost::bind(&Session::handleRead, self,
@@ -16,17 +20,18 @@ void Session::doRead() {
 
 void Session::handleRead(const boost::system::error_code& error, size_t bytes_transferred) {
     if (!error) {
+        Utilities::console_logger()->info("Session: Successfully read {} bytes", bytes_transferred);
         std::string received_data(data_, data_ + bytes_transferred);
         // Use MessageHandler to process the received data
         messageHandler.handleReceivedMessage(received_data);
-
         doWrite(bytes_transferred);
     } else {
-        std::cerr << "Error: " << error.message() << std::endl;
+        Utilities::console_logger()->error("Session Read Error: {}", error.message());
     }
 }
 
 void Session::doWrite(size_t length) {
+    Utilities::console_logger()->info("Session: Starting async_write with {} bytes", length);
     auto self(shared_from_this());
     boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
         boost::bind(&Session::handleWrite, self,
@@ -35,8 +40,9 @@ void Session::doWrite(size_t length) {
 
 void Session::handleWrite(const boost::system::error_code& error) {
     if (!error) {
+        Utilities::console_logger()->info("Session: Write completed, continuing to read");
         doRead();
     } else {
-        std::cerr << "Error: " << error.message() << std::endl;
+        Utilities::console_logger()->error("Session Write Error: {}", error.message());
     }
 }
