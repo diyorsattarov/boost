865655a1b39b42a49dfa358103a9ca93266f7dc7
diyooor <diyorsattarov7@gmail.com>
2023-12-13 13:02:24 -0600

    Added: - notifyConnected and waitForConnected methods - mutex for synchronization

--- include/client/client.h
+++ include/client/client.h
@@ -13,11 +13,25 @@ class Client {
     std::string server_response_;
     enum { max_length = 1024 };
     char data_[max_length];
+    std::mutex mtx;
+    std::condition_variable cv;
+    bool connected = false;
 
 public:
     Client(boost::asio::io_service& io_service, const std::string& host, const std::string& port);
 
     const std::string& server_response() const;
+
+    void notifyConnected() {
+        std::unique_lock<std::mutex> lock(mtx);
+        connected = true;
+        cv.notify_one();
+    }
+
+    void waitForConnection() {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, [this] { return connected; });
+    }
 
 private:
     void doConnect(const tcp::resolver::results_type& endpoints);
